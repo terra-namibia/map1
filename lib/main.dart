c130649b7c7eba774eb6049695908bd48e29a5b2
terra-namibia <tera9319@yahoo.co.jp>
2022-04-21 11:46:19 +0900

    add config for ios

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Google Maps Demo',
       home: MapSample(),
     );
@@ -16,43 +16,65 @@ class MyApp extends StatelessWidget {
 }
 
 class MapSample extends StatefulWidget {
+  const MapSample({Key? key}) : super(key: key);
+
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
 class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(35.6580339, 139.7016358),
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 0.0,
+      target: LatLng(35.6580339, 139.7016358),
+      // tilt: 59.440717697143555,
+      zoom: 14.4746);
+
+  static const LatLng _marker1 = LatLng(35.6510339, 139.7046358);
+  static const LatLng _marker2 = LatLng(35.6612339, 139.7003358);
+
+  Set<Marker> _createMarker() {
+    return {
+      const Marker(
+        markerId: MarkerId("marker_1"),
+        position: _marker1,
+        infoWindow: InfoWindow(title: "施設1", snippet: 'よい施設'),
+      ),
+      const Marker(
+        markerId: MarkerId("marker_2"),
+        position: _marker2,
+        infoWindow: InfoWindow(title: "施設2", snippet: 'そこそこの施設'),
+      ),
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        buildingsEnabled: false,
+        markers: _createMarker(),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+        onPressed: _goToHome,
+        label: const Text('Home'),
+        icon: const Icon(Icons.home),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToHome() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
